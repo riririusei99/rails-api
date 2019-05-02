4f60a39782ca6e0800b1a8c1998c7745d49bf570
riririusei99 <riry1313@gmail.com>
2019-05-02 06:23:41 +0000

    add card button & action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+  def purchase
+     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+     Payjp::Charge.create(
+        :amount => 3500,
+        :card => params['payjp-token'],
+        :currency => 'jpy'
+      )
+  end
+  
 end
